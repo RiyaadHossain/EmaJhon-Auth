294a78ee70818428ecee62b574ebe05a28e91e4a
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-10 17:37:40 +0600

    Files are ready

---
+++ src/components/LogIn/LogIn.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './LogIn.css'
+
+const LogIn = () => {
+    return (
+        <div>
+            <form>
+                <div>
+                    <label htmlFor=""></label>
+                    i
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default LogIn;
