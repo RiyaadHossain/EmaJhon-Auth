5ec809898d35ee1193715e8835d4c789dfdb4c92
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-10 20:14:16 +0600

    Log in Functionality added

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../Firebase.init';
 import './LogIn.css'
 
 const LogIn = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
     const [errors, setErrors] = useState('')
+    const navigate = useNavigate()
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
 
     const onEmailBlur = e => {
         setEmail(e.target.value)
@@ -17,7 +25,11 @@ const LogIn = () => {
 
     const onFormSubmit = e => {
         e.preventDefault()
-        
+        signInWithEmailAndPassword(email, password)
+    }
+
+    if (user) {
+        navigate('/shop')
     }
 
     return (
@@ -30,9 +42,10 @@ const LogIn = () => {
                 </div>
                 <div className='input-group'>
                     <label htmlFor="">Your Passowrd</label>
-                    <input onBlur={onPasswordBlur} type="email" name="" id="" />
+                    <input onBlur={onPasswordBlur} type="password" name="" id="" />
                 </div>
                 <div className='little-msg'><Link to={'/signup'}>New to Ema Jhon. <span className='color'> Sign Up</span></Link></div>
+                <p>{loading}</p>
                 <p style={{color: 'red'}}>{ errors }</p>
                 <p style={{color: 'red'}}>{ error }</p>
                 <input className='submit-btn' type="submit" value="Log In" />
