fe492a093e71a142f7f5131f18fc733d1c7ccea7
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-10 17:59:04 +0600

    Form set up

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './LogIn.css'
 
 const LogIn = () => {
     return (
-        <div>
+        <div className='form-container'>
             <form>
-                <div>
-                    <label htmlFor=""></label>
-                    i
+                <h2>Log In</h2>
+                <div className='input-group'>
+                    <label htmlFor="">Your Email</label>
+                    <input type="email" name="" id="" />
                 </div>
+                <div className='input-group'>
+                    <label htmlFor="">Your Passowrd</label>
+                    <input type="email" name="" id="" />
+                </div>
+                <div className='little-msg'><Link to={'/register'}>New to Ema Jhon. <span className='color'> Sign Up</span></Link></div>
+                <input className='submit-btn' type="submit" value="Sign Up" />
+
             </form>
         </div>
     );
